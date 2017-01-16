0e7b8b8e1a8f33d8854e80cd901284eee468a061
angquest <angquest@att.net>
2017-01-16 10:48:17 -0800

    add routing and states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,24 @@
-angular.module("blocJams", []);
+(function() {
+    function config($stateProvider, $localProvider) {
+        $localProvider
+            .html5Mode({
+                enable: true, //by setting this to true, it will disable the hashbang
+                requireBase: false //this is not required, however, it will avoid a common $location error
+        });
+        
+        $stateProvider
+            .state("landing", {
+                url: "/",
+                templateUrl: "/templates/landing.html"
+        })
+            .state("ablum", {
+                url: "/album",
+                templateUrl: "/templates/album.html"
+        });
+    }
+    
+    angular
+        .module("blocJams", ["ui.router"])
+        .config(config);
+    
+})();
