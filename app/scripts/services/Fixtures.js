10d0948d535049f887e7592f553d98f0da25c66c
angquest <angquest@att.net>
2017-01-23 13:12:13 -0800

    removed /app/ from links

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -7,11 +7,11 @@
             artist: 'Pablo Picasso',
             label: 'Cubism',
             year: '1881',
-            albumArtUrl: '/app/assets/images/album_covers/01.png',
+            albumArtUrl: '/assets/images/album_covers/01.png',
             songs: [
-                {title: 'I\'m so Blue', duration: 161.71, audioUrl: '/app/assets/music/blue'},
-                {title: 'Green with Envy', duration: 103.96, audioUrl: '/app/assets/music/green'},
-                {title: 'Lady in Red', duration: 268.45, audioUrl: '/app/assets/music/red'},
+                {title: 'I\'m so Blue', duration: 161.71, audioUrl: '/assets/music/blue'},
+                {title: 'Green with Envy', duration: 103.96, audioUrl: '/assets/music/green'},
+                {title: 'Lady in Red', duration: 268.45, audioUrl: '/assets/music/red'},
                 {title: 'Pink Flamingo', duration: 153.14, audioUrl: '/app/assets/music/pink'},
                 {title: 'Magenta, the Pup', duration: 374.22, audioUrl: '/app/assets/music/magenta'}
             ]
@@ -22,7 +22,7 @@
             artist: 'Guglielmo Marconi',
             label: 'EM',
             year: '1909',
-            albumArtUrl: '/app/assets/images/album_covers/20.png',
+            albumArtUrl: '/assets/images/album_covers/20.png',
             songs: [
                 {title: 'Hello, Operator?', duration: '1:01'},
                 {title: 'Ring, ring, ring', duration: '5:01'},
