acd882467e10267ad19f846c92b2c4992b0d8a77
angquest <angquest@att.net>
2017-01-23 22:14:35 -0800

    tried debugging injector error

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,10 @@
 (function() {
     function config($stateProvider, $localProvider) {
         $localProvider
-//            .html5Mode({
-//                enable: true, //by setting this to true, it will disable the hashbang
-//                requireBase: false //this is not required, however, it will avoid a common $location error
-//        }); //this is only to make the website look nice, however, it throws more errors than not
+            .html5Mode({
+                enable: true, //by setting this to true, it will disable the hashbang
+                requireBase: false //this is not required, however, it will avoid a common $location error
+        }); //this is only to make the website look nice, however, it throws more errors than not
         
         $stateProvider
             .state("landing", {
@@ -25,7 +25,7 @@
     }
     
     angular
-        .module("blocJams", ["ui.router"])
-        .config(config);
+        .module("blocJams", ["ui.router"]) // With UI-Router, an application can be in different states that determine what                                         to display when a user navigates to a specific route.
+        .config(config); //configures behavior and look of the app
     
 })();
